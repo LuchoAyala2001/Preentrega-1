57c1285c3fa7f539955942df8a1b13e2c7deec45
Luis Ayala <ayalaluis638@gmail.com>
2024-03-21 10:59:36 -0300

    Creando una interaccion para agregar productos al carrito de compras

--- index.js
+++ index.js
@@ -1,34 +1,6 @@
-function saltarlinea(){
-    document.write("<br>");
-    }
-    
-    function escribir(frase){
-    
-    document.write(frase);
-    
-    saltarlinea();
-    }
-    function calcularImc(peso,altura){
-    
-    
-    
-    return peso / (altura * altura);
-    }
-    
-    nombreInformado= prompt("Informe su nombre");
-    pesoInformado = prompt("Informe su peso");
-    alturaInformada = prompt("Informe su altura");
-    imcCalculado = calcularImc(pesoInformado,alturaInformada);
-    
-    if ( imcCalculado <19){
-    escribir(nombreInformado + " su imc es de " + imcCalculado+ " se encuentra flaco");  
-    }
-    if (19<imcCalculado && imcCalculado<25){
-    escribir(nombreInformado + " su imc es de " + imcCalculado+ " se encuentra en peso normal");  
-    }
-    if (25<imcCalculado && imcCalculado<30){
-    escribir(nombreInformado + " su imc es de " + imcCalculado+ " se encuentra en sobrepeso");  
-    }
-    if (imcCalculado>30){
-    escribir(nombreInformado + " su imc es de " + imcCalculado+ " se encuentra en obesidad");  
-    }
+const btnCarro = document.querySelector('.carro_icono')
+const todo = document.querySelector('.todo')
+
+btnCarro.addEventListener('click', ()=> {
+    todo.classList.toggle('ocultar_carro')
+})
